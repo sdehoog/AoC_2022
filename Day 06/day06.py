3611db3b8489491f241e8534f2fae138ba1e4e81
steven <steven.dehoog@gmail.com>
2022-12-06 07:37:57 -0600

    Day 6

---
+++ Day 06/day06.py
@@ -0,0 +1,20 @@
+def day06(filepath, unique_length):
+    with open(filepath) as fin:
+        line = fin.readline()
+
+    for i in range(len(line)):
+        fourset = line[i:i+unique_length]
+        if all([fourset.count(line[j]) == 1 for j in range(i, i+unique_length)]):
+            return i + unique_length
+
+
+def main():
+    assert day06('test06', 4) == 7
+    print(day06('input06', 4))
+
+    assert day06('test06', 14) == 19
+    print(day06('input06', 14))
+
+
+if __name__ == '__main__':
+    main()
