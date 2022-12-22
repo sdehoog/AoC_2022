e6f5f6f504e4263fc39ccbaf35b2f47695eb5cea
steven <steven.dehoog@gmail.com>
2022-12-21 18:24:59 -0600

    Day 21 Part 1 working and Part 2 Test working

---
+++ Day 21/day21.py
@@ -0,0 +1,88 @@
+from time import time
+from z3 import *
+
+
+def timer_func(func):
+    # This function shows the execution time of
+    # the function object passed
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f'Function {func.__name__!r} executed in {(t2 - t1):.4f}s')
+        return result
+
+    return wrap_func
+
+
+def monkey_eval(monkey_name, monkey_dict):
+    monkey_yell = monkey_dict[monkey_name]
+    if isinstance(monkey_yell, int):
+        return monkey_yell
+    else:
+        if monkey_yell[1] == '/':
+            return monkey_eval(monkey_yell[0], monkey_dict) / monkey_eval(monkey_yell[2], monkey_dict)
+        if monkey_yell[1] == '+':
+            return monkey_eval(monkey_yell[0], monkey_dict) + monkey_eval(monkey_yell[2], monkey_dict)
+        if monkey_yell[1] == '-':
+            return monkey_eval(monkey_yell[0], monkey_dict) - monkey_eval(monkey_yell[2], monkey_dict)
+        if monkey_yell[1] == '*':
+            return monkey_eval(monkey_yell[0], monkey_dict) * monkey_eval(monkey_yell[2], monkey_dict)
+        if monkey_yell[1] == '=':
+            return monkey_eval(monkey_yell[0], monkey_dict) == monkey_eval(monkey_yell[2], monkey_dict)
+
+
+@timer_func
+def day21(filepath, part2=False):
+    with open(filepath) as fin:
+        lines = fin.readlines()
+
+    monkey_dict = {}
+    for line in lines:
+        monkey_name, monkey_yell = [x.strip() for x in line.split(':')]
+        monkey_yell = [x for x in monkey_yell.split()]
+        if len(monkey_yell) == 1:
+            monkey_yell = int(monkey_yell[0])
+        monkey_dict[monkey_name] = monkey_yell
+
+    if not part2:
+        return int(monkey_eval('root', monkey_dict))
+    else:
+        vals = {}
+        s = Solver()
+        for key in monkey_dict.keys():
+            vals[key] = Int(key)
+        for key, val in monkey_dict.items():
+            if key == 'humn':
+                continue
+            elif key == 'root':
+                s.add(vals[val[0]] == vals[val[2]])
+            else:
+                if isinstance(val, int):
+                    s.add(vals[key] == val)
+                else:
+                    a, m, b = val
+                    if m == '+':
+                        s.add(vals[key] == (vals[a] + vals[b]))
+                    elif m == '-':
+                        s.add(vals[key] == (vals[a] - vals[b]))
+                    elif m == '*':
+                        s.add(vals[key] == (vals[a] * vals[b]))
+                    elif m == '/':
+                        s.add(vals[key] == (vals[a] / vals[b]))
+
+        s.check()
+        m = s.model()
+        return m[vals['humn']]
+
+
+def main():
+    assert day21('test21') == 152
+    print(f"Part 1: {day21('input21')}")
+
+    assert day21('test21', True) == 301
+    print(f"Part 2: {day21('input21', True)}")
+
+
+if __name__ == '__main__':
+    main()
